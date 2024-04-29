29d4493e4dea514ce9e19fa903609ae5289e903f
Enes Esvet Kuzucu <eneskuzucu@hotmail.com>
2024-04-29 13:16:55 +0300

    m

--- facexformer/inference_cpu.py
+++ facexformer/inference_cpu.py
@@ -1,11 +1,4 @@
-"""
-Face Parsing - Task 0
-Face Landmarks Detection - Task 1
-Face Headpose Estimation - Task 2
-Face Attributes Recognition - Task 3
-Face Age/Gender/Race Estimation - Task 4
-Face Landmarks Visibility Prediction - Task 5
-"""
+
 import os
 import sys
 
@@ -27,255 +20,111 @@ from math import cos, sin
 from PIL import Image
 from network import FaceXFormer
 from facenet_pytorch import MTCNN
-
+from facexformer_utils import denorm_points, unnormalize, adjust_bbox, visualize_head_pose, visualize_landmarks, visualize_mask
+from task_postprocesser import task_faceparsing,     process_landmarks, task_headpose , task_attributes, task_gender, process_visibility
 
 import os
 
 
 
-def visualize_mask(image_tensor, mask):
-    image = image_tensor.numpy().transpose(1, 2, 0) * 255 
-    image = image.astype(np.uint8)
-    
-    color_mask = np.zeros((mask.shape[0], mask.shape[1], 3), dtype=np.uint8)
-    color_mapping = np.array([
-        [0, 0, 0],
-        [0, 153, 255],
-        [102, 255, 153],
-        [0, 204, 153],
-        [255, 255, 102],
-        [255, 255, 204],
-        [255, 153, 0],
-        [255, 102, 255],
-        [102, 0, 51],
-        [255, 204, 255],
-        [255, 0, 102]
-    ])
-    
-    for index, color in enumerate(color_mapping):
-        color_mask[mask == index] = color
+def load_model(weights_path):
+    gpu_num = 0
+    device = torch.device("cuda:" + str(gpu_num) if torch.cuda.is_available() else "cpu")
 
-    overlayed_image = cv2.addWeighted(image, 0.5, color_mask, 0.5, 0)
-
-    return overlayed_image, image, color_mask
-
-def visualize_landmarks(im, landmarks, color, thickness=3, eye_radius=0):
-    im = im.permute(1, 2, 0).numpy()
-    im = (im * 255).astype(np.uint8)
-    im = np.ascontiguousarray(im)
-    landmarks = landmarks.squeeze().numpy().astype(np.int32)
-    for (x, y) in landmarks:
-        cv2.circle(im, (x,y), eye_radius, color, thickness)
-    return im
-
-def visualize_head_pose(img, euler, tdx=None, tdy=None, size = 100):
-    pitch, yaw, roll = euler[0], euler[1], euler[2]
-
-    img = img.permute(1, 2, 0).numpy()
-    img = (img * 255).astype(np.uint8)
-    img = np.ascontiguousarray(img)
-
-    if tdx != None and tdy != None:
-        tdx = tdx
-        tdy = tdy
-    else:
-        height, width = img.shape[:2]
-        tdx = width / 2
-        tdy = height / 2
-
-    # X-Axis pointing to right. drawn in red
-    x1 = size * (cos(yaw) * cos(roll)) + tdx
-    y1 = size * (cos(pitch) * sin(roll) + cos(roll) * sin(pitch) * sin(yaw)) + tdy
-    # Y-Axis | drawn in green
-    #        v
-    x2 = size * (-cos(yaw) * sin(roll)) + tdx
-    y2 = size * (cos(pitch) * cos(roll) - sin(pitch) * sin(yaw) * sin(roll)) + tdy
-    # Z-Axis (out of the screen) drawn in blue
-    x3 = size * (sin(yaw)) + tdx
-    y3 = size * (-cos(yaw) * sin(pitch)) + tdy
-
-    cv2.line(img, (int(tdx), int(tdy)), (int(x1),int(y1)),(0,255,255),3)
-    cv2.line(img, (int(tdx), int(tdy)), (int(x2),int(y2)),(255,255,0),3)
-    cv2.line(img, (int(tdx), int(tdy)), (int(x3),int(y3)),(255,0,255),2)
-    return img
-
-def denorm_points(points, h, w, align_corners=False):
-    if align_corners:
-        denorm_points = (points + 1) / 2 * torch.tensor([w - 1, h - 1], dtype=torch.float32).to(points).view(1, 1, 2)
-    else:
-        denorm_points = ((points + 1) * torch.tensor([w, h], dtype=torch.float32).to(points).view(1, 1, 2) - 1) / 2
-
-    return denorm_points
-
-
-
-def unnormalize(tensor, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
-    mean = torch.tensor(mean).view(-1, 1, 1)
-    std = torch.tensor(std).view(-1, 1, 1)
-    tensor = tensor * std + mean 
-    tensor = torch.clamp(tensor, 0, 1)
-    return tensor
-
-def adjust_bbox(x_min, y_min, x_max, y_max, image_width, image_height, margin_percentage=50):
-    width = x_max - x_min
-    height = y_max - y_min
-    
-    increase_width = width * (margin_percentage / 100.0) / 2
-    increase_height = height * (margin_percentage / 100.0) / 2
-    
-    x_min_adjusted = max(0, x_min - increase_width)
-    y_min_adjusted = max(0, y_min - increase_height)
-    x_max_adjusted = min(image_width, x_max + increase_width)
-    y_max_adjusted = min(image_height, y_max + increase_height)
-    
-    return x_min_adjusted, y_min_adjusted, x_max_adjusted, y_max_adjusted
-
-
-def test(args):
-    # device = "cuda:" + str(args.gpu_num)
-    device = torch.device("cuda:" + str(args.gpu_num) if torch.cuda.is_available() else "cpu")
-    print(f"Running on {device}")
     model = FaceXFormer().to(device)
-    weights_path = args.model_path
-    # weights_path= "/Users/ns/.cache/torch/hub/checkpoints/swin_b-68c6b09e.pth"
-
-    checkpoint = torch.load(weights_path, map_location=device)
-    #print("Checkpoint keys: ", checkpoint.keys())
-    # model.load_state_dict(checkpoint['state_dict_backbone'])
-    new_state_dict = {}
-    for key, value in checkpoint.items():
-        new_key = key
-        # Modify the key names based on the expected structure of your current model
-        if 'features' in key:
-            new_key = key.replace('features',
-                                  'backbone')  # Example adjustment, customize based on your model's architecture
-        new_state_dict[new_key] = value
-
-    # Load the adjusted state dict
     try:
-        model.load_state_dict(new_state_dict)
+        checkpoint = torch.load(weights_path, map_location=device)
+        model.load_state_dict(checkpoint['state_dict_backbone'])
         print("Model loaded successfully.")
-    except RuntimeError as e:
+    except Exception as e:
         print("Failed to load model:", e)
-
-    # model.load_state_dict(checkpoint)
-
+        return None
     model.eval()
+    return model
+
+
+def test(model, image_path):
+
+    device = torch.device("cuda:" + str(gpu_num) if torch.cuda.is_available() else "cpu")
+
     transforms_image = torchvision.transforms.Compose([
                 torchvision.transforms.Resize(size=(224,224), interpolation=InterpolationMode.BICUBIC),
                 torchvision.transforms.ToTensor(),
                 torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ])
     mtcnn = MTCNN(keep_all=True)
-    image = Image.open(args.image_path)
+    image = Image.open(image_path)
     width, height = image.size
     boxes, probs = mtcnn.detect(image)
     x_min, y_min, x_max, y_max = boxes[0][0], boxes[0][1], boxes[0][2], boxes[0][3]
     x_min, y_min, x_max, y_max = adjust_bbox(x_min, y_min, x_max, y_max, width, height)
+
     image = image.crop((int(x_min), int(y_min), int(x_max), int(y_max)))
+    print("image size after crop", image.size)
+    # (346, 438)
+
     image = transforms_image(image)
 
-    if args.task == "parsing":
-        task = torch.tensor([0])
-    elif args.task == "landmarks":
-        task = torch.tensor([1])
-    elif args.task == "headpose":
-        task = torch.tensor([2])
-    elif args.task == "attributes":
-        task = torch.tensor([3])
-    elif args.task == "age_gender_race":
-        task = torch.tensor([4])
-    elif args.task == "visibility":
-        task = torch.tensor([5])
-    data = {'image': image, 'label': {"segmentation":torch.zeros([224,224]), "lnm_seg": torch.zeros([5, 2]),"landmark": torch.zeros([68, 2]), "headpose": torch.zeros([3]), "attribute": torch.zeros([40]), "a_g_e": torch.zeros([3]), 'visibility': torch.zeros([29])}, 'task': task}
-    images, labels, tasks = data["image"], data["label"], data["task"]
-    images = images.unsqueeze(0).to(device=device)
+    label_shapes = {
+        "segmentation": (224, 224),
+        "lnm_seg": (5, 2),
+        "landmark": (68, 2),
+        "headpose": (3,),
+        "attribute": (40,),
+        "a_g_e": (3,),
+        "visibility": (29,)
+    }
+    labels = {key: torch.zeros(shape) for key, shape in label_shapes.items()}
+    data = {
+        "image": image,
+        "label": labels,
+    }
+
+    # images, labels, tasks = data["image"], data["label"], data["task"]
+    images, labels = data["image"], data["label"]
     for k in labels.keys():
         labels[k] = labels[k].unsqueeze(0).to(device=device)
-    tasks = tasks.to(device=device)
+    images = images.unsqueeze(0).to(device=device)
 
-    landmark_output, headpose_output, attribute_output, visibility_output, age_output, gender_output, race_output, seg_output = model(images, labels, tasks)
-    if tasks[0] == 0:
-        preds = seg_output.softmax(dim=1)
-        mask = torch.argmax(preds, dim=1)
-        pred_mask = mask[0].detach().cpu().numpy()
-        save_path = os.path.join(args.results_path, "parsing.png")
-        cv2.imwrite(f"{save_path}", pred_mask)
-        mask, face, color_mask = visualize_mask(unnormalize(images[0].detach().cpu()), pred_mask)
-        save_path = os.path.join(args.results_path, "parsing_visualization.png")
-        cv2.imwrite(f"{save_path}", mask[:, :, ::-1])
-    if tasks[0] == 1:
-        image = unnormalize(images[0].detach().cpu())
-        denorm_landmarks = denorm_points(landmark_output.view(-1,68,2)[0],224,224)
-        im = visualize_landmarks(image, denorm_landmarks.detach().cpu(), (255, 255, 0))
-        save_path_viz = os.path.join(args.results_path, "landmarks.png")
-        save_path = os.path.join(args.results_path, "landmarks.txt")
-        cv2.imwrite(f"{save_path_viz}", im[:, :, ::-1])
-        with open(f'{save_path}', 'w') as file:
-            for landmark in denorm_landmarks[0]:
-                x, y = landmark[0], landmark[1]
-                file.write(f"{x.item()} {y.item()}\n")
-        file.close()
-    if tasks[0] == 2:
-        image = unnormalize(images[0].detach().cpu())
-        im = visualize_head_pose(image, headpose_output[0])
-        save_path_viz = os.path.join(args.results_path, "headpose.png")
-        save_path = os.path.join(args.results_path, "headpose.txt")
-        cv2.imwrite(f"{save_path_viz}", im[:, :, ::-1])
-        with open(f'{save_path}', 'w') as file:
-            file.write(f"Pitch: {headpose_output[0][0].item()*180/np.pi} \n")
-            file.write(f"Yaw: {headpose_output[0][1].item()*180/np.pi} \n")
-            file.write(f"Roll: {headpose_output[0][2].item()*180/np.pi}")
-        file.close()
-    if tasks[0] == 3:
-        probs = torch.sigmoid(attribute_output[0])
-        preds = (probs >= 0.5).float()
-        pred = preds.tolist()
-        pred_str = [str(int(b)) for b in pred]
-        joined_pred = " ".join(pred_str)
-        save_path = os.path.join(args.results_path, "attribute.txt")
-        with open(f'{save_path}', 'w') as file:
-            file.write(joined_pred)
-        file.close()
-    if tasks[0] == 4:
-        age_preds = torch.argmax(age_output, dim=1)[0]
-        gender_preds = torch.argmax(gender_output, dim=1)[0]
-        race_preds = torch.argmax(race_output, dim=1)[0]
-        save_path = os.path.join(args.results_path, "age_gender_race.txt")
-        with open(f'{save_path}', 'w') as file:
-            file.write(f"Age: {age_preds.item()} \n")
-            file.write(f"Gender: {gender_preds.item()} \n")
-            file.write(f"Race: {race_preds.item()}")
-        file.close()
-    if tasks[0] == 5:
-        probs = torch.sigmoid(visibility_output[0])
-        preds = (probs >= 0.5).float()
-        pred = preds.tolist()
-        pred_str = [str(int(b)) for b in pred]
-        joined_pred =  " ".join(pred_str)
-        save_path = os.path.join(args.results_path, "visibility.txt")
-        with open(f'{save_path}', 'w') as file:
-            file.write(joined_pred)
-        file.close()
+
+    for i in range(6):
+         task = torch.tensor([i])
+         task = task.to(device=device)
+         output = model(images, labels, task)
+
+         for e in output:
+            print(e.shape)
+         landmark_output, headpose_output, attribute_output, visibility_output, age_output, gender_output, race_output, seg_output =output
+         if task[0] == 0:
+             faceparsing_mask=task_faceparsing(seg_output)
+         if task[0] == 1:
+             landmark_dict=process_landmarks(landmark_output,images )
+         if task[0] == 2:
+             headpose_dict=task_headpose(headpose_output, images)
+         if task[0] == 3:
+             attributes=task_attributes(attribute_output)
+         if task[0] == 4:
+             age_gender_race_dict=task_gender(age_output,gender_output,race_output )
+         if task[0] == 5:
+             visibility_result=process_visibility(visibility_output)
+
+
     image = unnormalize(images[0].detach().cpu())
     image = image.permute(1, 2, 0).numpy()
     image = (image * 255).astype(np.uint8)
-    save_path = os.path.join(args.results_path, "face.png")
-    cv2.imwrite(f"{save_path}", image[:, :, ::-1])
+    image =image[:, :, ::-1]
 
-
-            
-
-
-
-    
+    # save_path = os.path.join(results_path, "face.png")
+    # cv2.imwrite(f"{save_path}", image[:, :, ::-1])
+    return image, faceparsing_mask, landmark_dict, headpose_dict, attributes, age_gender_race_dict, visibility_result
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model_path", type=str, help="Provide absolute path to your weights file")
-    parser.add_argument("--image_path", type=str, help="Provide absolute path to the image you want to perform inference on")
-    parser.add_argument("--results_path", type=str, help="Provide path to the folder where results need to be saved")
-    parser.add_argument("--task", type=str, help="parsing" or "landmarks" or "headpose" or "attributes" or "age_gender_race" or "visibility")
-    parser.add_argument("--gpu_num", type=str, help="Provide the gpu number")
-    args = parser.parse_args()  
-    test(args)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--model_path", type=str, help="Provide absolute path to your weights file")
+    # parser.add_argument("--image_path", type=str, help="Provide absolute path to the image you want to perform inference on")
+    # parser.add_argument("--results_path", type=str, help="Provide path to the folder where results need to be saved")
+    # parser.add_argument("--task", type=str, help="parsing" or "landmarks" or "headpose" or "attributes" or "age_gender_race" or "visibility")
+    # parser.add_argument("--gpu_num", type=str, help="Provide the gpu number")
+    # args = parser.parse_args()
+    # test(args, model_path,image_path, results_path  )
+    pass
--- main.py
+++ main.py
@@ -1,37 +1,45 @@
 
 
 import subprocess
-from facexformer.inference_cpu import test
+# from facexformer.inference_cpu import test
+# from facexformer.inference2 import calculate_facexformer_outputs
+from facexformer.inference_cpu import test, load_model
 import argparse
+
 import glob
 import shutil
 
 import certifi
 import os
 
-os.environ['SSL_CERT_FILE'] = certifi.where()
+from UniversalImageHandler import UniversalImageHandler
 
+# pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu
 
-tasks = ["parsing", "landmarks", "headpose", "attributes", "age_gender_race", "visibility"]
+# os.environ['SSL_CERT_FILE'] = certifi.where()
+
+# tasks = ["parsing", "landmarks", "headpose", "attributes", "age_gender_race", "visibility"]
+tasks = ["parsing", "landmarks", "headpose"]
 def main():
-    # Use a breakpoint in the code line below to debug your script.
-    # print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-    image_path= "img3.jpg"
-    # results_path="./"
-    results_path="/Users/ns/Desktop/projects/faceformer_deneme/"
-    for task in tasks:
-        flag = 1
-        args = {
+    # hf_hub_download(repo_id="kartiknarayan/facexformer", filename="ckpts/model.pt", local_dir="./")
 
-            "model_path": "/Users/ns/.cache/torch/hub/checkpoints/swin_b-68c6b09e.pth",
-            "image_path": image_path,
-            "results_path": results_path,
-            "task": task,
-            "gpu_num": "0"
-        }
-        args_namespace = argparse.Namespace(**args)
-        test(args_namespace)
+
+    image_path = "img3.jpg"
+    uih=UniversalImageHandler(image_path, debug=False)
+    COMPATIBLE, img =   uih.COMPATIBLE, uih.img
+    # print("COMPATIBLE:", COMPATIBLE)
+    # print("shape img :", img.shape)
+    # task_types= ["parsing", "landmarks", "headpose", "attributes", "age_gender_race", "visibility"]
+    # calculate_facexformer_outputs(model_path, img, task_types,  device='cpu')
+
+
+    image_path = "img3.jpg"
+    model_path= "ckpts/model.pt"
+    model=load_model(model_path)
+    # results_path= "./results"
+    image, faceparsing_mask, landmark_dict, headpose_dict, attributes, age_gender_race_dict, visibility_result= test(model, image_path)
+
 
 
 if __name__ == '__main__':
